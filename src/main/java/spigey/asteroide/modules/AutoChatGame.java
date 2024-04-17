39e3fd26952ff93262ecf80f6cd7c988349223df
DieZeitungsente <younes.haika@gmail.com>
2024-04-17 21:17:46 +0200

    massive fixes

--- src/main/java/spigey/asteroide/modules/AutoChatGame.java
+++ src/main/java/spigey/asteroide/modules/AutoChatGame.java
@@ -24,6 +24,7 @@ public class AutoChatGame extends Module {
     private final SettingGroup sgGeneral = settings.getDefaultGroup();
 
     private final Setting<List<String>> messages = sgGeneral.add(new StringListSetting.Builder().name("messages").description("Trigger messages").defaultValue("say", "type", "write").build());
+    private final Setting<List<String>> quotes = sgGeneral.add(new StringListSetting.Builder().name("quotes").description("Quotes").defaultValue("\"", "'", "`").build());
 
     public AutoChatGame() {
         super(AsteroideAddon.CATEGORY, "auto-chatgame", "Automatically answers 'say [...]' chat games when triggered");
@@ -31,31 +32,25 @@ public class AutoChatGame extends Module {
 
     @EventHandler(priority = EventPriority.HIGHEST + 1)
     private void PacketReceive(PacketEvent.Receive event) {
-        if (!(event.packet instanceof GameMessageS2CPacket)) {
-            return;
-        }
-        if (((GameMessageS2CPacket) event.packet).content().toString().contains(mc.getSession().getUsername())) {
-            return;
-        }
+        if (!(event.packet instanceof GameMessageS2CPacket)) {return;}
+        String content = String.valueOf(((GameMessageS2CPacket) event.packet).content().getString());
         boolean yes = false;
-        boolean no = !yes; // no = not yes;
-        for (int i = 0; i < messages.get().size(); i++) {
-            if (((GameMessageS2CPacket) event.packet).content().toString().contains(messages.get().get(i))) {
-                yes = true;
-            }
-            no = !yes; // once again does not equal yes
+        boolean no = false;
+        boolean reverse = false;
+        String quote = "";
+        for(int i = 0; i < messages.get().size(); i++){
+            if(content.toLowerCase().contains(messages.get().get(i).toLowerCase())){yes = true;}
+            if(content.toLowerCase().contains("reverse")){yes = true; reverse = true;}
         }
-        if (no) {
-            return;
-        } // I just fucking felt like it
-        if (!(((GameMessageS2CPacket) event.packet).content().toString().contains("\"") || ((GameMessageS2CPacket) event.packet).content().toString().contains("'"))) {
-            return;
-        } // help wtf is this does it even work??
-        String fuckingkys = ((GameMessageS2CPacket) event.packet).content().toString()
-            .split("literal")[2];
-        String kys2 = fuckingkys
-            .split(((GameMessageS2CPacket) event.packet).content().toString().contains("'") ? "'" : "\"")[0];
-        if(kys2.substring(1, kys2.indexOf("}")).length() <= 1){return;}
-        msg(kys2.substring(1, kys2.indexOf("}")));
+        for(int i = 0; i < quotes.get().size(); i++){
+            if(content.toLowerCase().contains(quotes.get().get(i))){no = true; quote = quotes.get().get(i);}
+        }
+        if(yes && no){
+            if(reverse){
+                msg(String.valueOf(new StringBuilder(content.split(quote)[1]).reverse()));
+            } else {
+                msg(content.split(quote)[1]);
+            }
+        }
     }
 }
