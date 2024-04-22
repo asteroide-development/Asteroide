a9d951789b1afe9405f66a751d5951251c4ee5bf
DieZeitungsente <younes.haika@gmail.com>
2024-04-22 22:05:34 +0200

    kms

---
+++ src/main/java/spigey/asteroide/events/SendMessageEvent.java
@@ -0,0 +1,15 @@
+package spigey.asteroide.events;
+
+import meteordevelopment.meteorclient.events.Cancellable;
+
+public class SendMessageEvent extends Cancellable {
+    private static final SendMessageEvent INSTANCE = new SendMessageEvent();
+
+    public String message;
+
+    public static SendMessageEvent get(String message) {
+        INSTANCE.setCancelled(false);
+        INSTANCE.message = message;
+        return INSTANCE;
+    }
+}
