11c44336fa0f3953942e5de89a9a01a6dd6eae63
DieZeitungsente <younes.haika@gmail.com>
2024-04-25 12:09:49 +0200

    now supports variable games

--- src/main/java/spigey/asteroide/modules/AutoChatGame.java
+++ src/main/java/spigey/asteroide/modules/AutoChatGame.java
@@ -21,13 +21,25 @@ import static spigey.asteroide.util.msg;
 public class AutoChatGame extends Module {
 
     private final SettingGroup sgGeneral = settings.getDefaultGroup();
-
+    private final Setting<Boolean> experimental = sgGeneral.add(new BoolSetting.Builder()
+        .name("Enable experimental features")
+        .description("Enables some features that are still in development")
+        .defaultValue(false)
+        .build()
+    );
     private final Setting<List<String>> messages = sgGeneral.add(new StringListSetting.Builder().name("messages").description("Trigger messages").defaultValue("say", "type", "write").build());
     private final Setting<List<String>> reversers = sgGeneral.add(new StringListSetting.Builder().name("reversers").description("Strings that will reverse the solution").defaultValue("reverse").build());
-    private final Setting<List<String>> quotes = sgGeneral.add(new StringListSetting.Builder().name("quotes").description("Quotes").defaultValue("\"", "'", "`").build());
-    private final Setting<List<String>> dont = sgGeneral.add(new StringListSetting.Builder().name("blacklisted messages").description("Do not solve the chatgame if it contains one of these Strings").defaultValue("solved", "successfully").build());
     private final Setting<List<String>> mether = sgGeneral.add(new StringListSetting.Builder().name("mather").description("Strings that will solve an equation as solution").defaultValue("solve", "equation", "calculate").build());
+    private final Setting<List<String>> variablegame = sgGeneral.add(new StringListSetting.Builder()
+        .name("variable game")
+        .description("Strings that will trigger the variable game")
+        .defaultValue("✗", "solve for")
+        .visible(() -> experimental.get())
+        .build()
+    );
+    private final Setting<List<String>> quotes = sgGeneral.add(new StringListSetting.Builder().name("quotes").description("Quotes").defaultValue("\"", "'", "`").build());
     private final Setting<List<String>> contain = sgGeneral.add(new StringListSetting.Builder().name("must contain").description("Requires the message to contain all of these Strings").defaultValue().build());
+    private final Setting<List<String>> dont = sgGeneral.add(new StringListSetting.Builder().name("blacklisted messages").description("Do not solve the chatgame if it contains one of these Strings").defaultValue("solved", "successfully").build());
     private final Setting<Mode> mode = sgGeneral.add(new EnumSetting.Builder<Mode>()
         .name("delay type")
         .description("Whether it waits for a random or precise amount of time")
@@ -83,12 +95,19 @@ public class AutoChatGame extends Module {
     private String solution = "";
     private boolean subscribed = false;
     private Random rand = new Random();
+    private String OutputMode = "send";
     public AutoChatGame() {
         super(AsteroideAddon.CATEGORY, "auto-chatgame", "Automatically answers most chat games when triggered");
     }
     int meth(String equation) {
         return (int) util.eval(equation); // I was too lazy to actually change the method everywhere
     }
+
+    @Override
+    public void onActivate() {
+        this.tick = -1;
+    }
+
     @EventHandler(priority = EventPriority.HIGHEST + 2)
     private void PacketReceive(PacketEvent.Receive event) {
         if (!(event.packet instanceof GameMessageS2CPacket)) {return;}
@@ -97,6 +116,7 @@ public class AutoChatGame extends Module {
         boolean no = false;
         boolean reverse = false;
         boolean dometh = false;
+        boolean vargame = false;
         String quote = "";
         for(int i = 0; i < messages.get().size(); i++){
             if(content.toLowerCase().contains(messages.get().get(i).toLowerCase())){yes = true;}
@@ -120,6 +140,10 @@ public class AutoChatGame extends Module {
         for(int i = 0;i < isit.length; i++){
             if(!isit[i]){yes = false;}
         }
+        for(int i = 0; i < variablegame.get().size(); i++){
+            if(!experimental.get()){return;}
+            if(content.toLowerCase().contains(variablegame.get().get(i))){yes = true; dometh = false; reverse = false; vargame = true;}
+        }
         if(yes && no){
             if(reverse){
                 solution = String.valueOf(new StringBuilder(content.split(quote)[1]).reverse());
@@ -141,6 +165,8 @@ public class AutoChatGame extends Module {
                     solution = String.valueOf(themeth);
                 }
 
+            } else if(vargame){
+               solution = String.valueOf(DoVariableGame(content));
             } else {
                 solution = content.split(quote)[1];
             }
@@ -152,7 +178,6 @@ public class AutoChatGame extends Module {
                     ChatUtils.sendMsg(Text.of("§7[§9Chatgame§7]§f " + mode + ": " + quote + content.split(quote)[1] + quote + "!"));
                 }
             }
-            if(showsul.get()){info("[\uD83D\uDEC8] §fThe solution is " + solution + ".§r");} else{
                 if(mode.get() == Mode.Precise){
                     this.tick = delay.get();
                 } else if(mode.get() == Mode.Random){
@@ -160,13 +185,26 @@ public class AutoChatGame extends Module {
                     if(Math.random() > 0.5){randomshit -= randomshit * 2;}
                     this.tick = delay.get() + randomshit;
                 }
+                if(showsul.get()){
+                    this.OutputMode = "show";
+                    this.solution = "§7[§f\uD83D\uDEC8§7] §fThe solution is " + solution + ".§r";
+                    this.tick = 1;
+                }
                 if(!subscribed){
                     MeteorClient.EVENT_BUS.subscribe(this);
                     subscribed = true;
                 }
-            }
         }
 
+    }
+    int DoVariableGame(String output){ // No, this is actually not AI generated.
+        String[] equations = output.split("\n");
+
+        int Z = Integer.parseInt(equations[2].split(" = ")[1]);
+        int B = Integer.parseInt(equations[3].split(" = ")[1]);
+        int N = Integer.parseInt(equations[4].split(" = ")[1]);
+
+        return N - ((Z / 3) + (B / 3));
     }
 
     @EventHandler
@@ -174,7 +212,11 @@ public class AutoChatGame extends Module {
         if(!isActive()){return;} // this got me banned off my favorite server
         if(this.tick > 0){this.tick--; return;} // don't execute when it's not done waiting
         if(this.tick == -1){return;} // disable when on -1
-        msg(this.solution);
+        if (this.OutputMode == "send") {
+            msg(this.solution);
+        } else{
+            ChatUtils.sendMsg(Text.of(this.solution));
+        }
         this.tick = -1;
     }
 
