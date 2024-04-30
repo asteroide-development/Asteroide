ddd4b63192db8e031b662e1ac2e9460992f964b5
DieZeitungsente <younes.haika@gmail.com>
2024-04-30 20:22:00 +0200

    small fix

--- src/main/java/spigey/asteroide/commands/CommandBlockCommand.java
+++ src/main/java/spigey/asteroide/commands/CommandBlockCommand.java
@@ -28,7 +28,7 @@ public class CommandBlockCommand extends Command {
             if(!mc.player.getAbilities().creativeMode){error("You need to be in creative mode to use this command!"); return SINGLE_SUCCESS;}
             if(!mc.player.hasPermissionLevel(4)){error("You're missing the permission level '§f4§c', you can most likely not place the command block!");}
             ChatUtils.sendMsg(Text.of("§fReceiving command block with command '§7" + StringArgumentType.getString(context,"command") + "§f'."));
-            CommandBlock(Items.COMMAND_BLOCK, StringArgumentType.getString(context, "command"), 1);
+            CommandBlock(Items.COMMAND_BLOCK, StringArgumentType.getString(context, "command").replaceAll("\"", "\\\\\\\""), 1);
             return SINGLE_SUCCESS;
         }));
     }
