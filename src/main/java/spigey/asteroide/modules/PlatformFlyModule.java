ebe0ba9c69da3c7041559903607d1674ccbccf5d
DieZeitungsente <younes.haika@gmail.com>
2024-05-07 23:20:34 +0200

    v14

--- src/main/java/spigey/asteroide/modules/PlatformFlyModule.java
+++ src/main/java/spigey/asteroide/modules/PlatformFlyModule.java
@@ -3,9 +3,12 @@ package spigey.asteroide.modules;
 import meteordevelopment.meteorclient.events.meteor.KeyEvent;
 import meteordevelopment.meteorclient.events.world.TickEvent;
 import meteordevelopment.meteorclient.systems.modules.Module;
+import meteordevelopment.meteorclient.utils.misc.input.Input;
 import meteordevelopment.meteorclient.utils.misc.input.KeyAction;
 import meteordevelopment.orbit.EventHandler;
+import net.minecraft.client.util.InputUtil;
 import net.minecraft.network.packet.c2s.play.PlayerMoveC2SPacket;
+import org.lwjgl.glfw.GLFW;
 import spigey.asteroide.AsteroideAddon;
 
 public class PlatformFlyModule extends Module {
@@ -21,17 +24,11 @@ public class PlatformFlyModule extends Module {
         level = mc.player.getBlockPos().getY();
     }
     @EventHandler
-    private void onKey(KeyEvent event){
-        if(event.action != KeyAction.Press && event.action != KeyAction.Repeat) return;
-        if(mc.options.sneakKey.matchesKey(event.key, 0)){level--; mc.player.setVelocity(mc.player.getVelocity().x, -1, mc.player.getVelocity().z); return;}
-        if(!mc.options.jumpKey.matchesKey(event.key, 0)){level++; return;}
-        assert mc.player != null;
-        if(mc.player.isOnGround()) level = mc.player.getBlockPos().getY();
-    }
-    @EventHandler
     private void onTick(TickEvent.Pre event){
         assert mc.player != null;
-        if(mc.player.isOnGround()) return;
+        level = mc.player.getBlockPos().getY();
+        if(Input.isKeyPressed(GLFW.GLFW_KEY_SPACE)){level++;}
+        if(Input.isKeyPressed(GLFW.GLFW_KEY_LEFT_SHIFT)){level--;}
         if(mc.player.getBlockPos().getY() <= level){
             /* mc.player.networkHandler.sendPacket(new PlayerMoveC2SPacket.PositionAndOnGround(mc.player.getX(), level, mc.player.getZ(), true));
             mc.player.setPosition(mc.player.getX(), level, mc.player.getZ()); */
