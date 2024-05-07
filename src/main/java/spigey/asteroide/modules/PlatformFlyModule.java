015fe58753d2840be7b8ae074d1adbab7b5f9b0e
DieZeitungsente <younes.haika@gmail.com>
2024-05-08 00:17:03 +0200

    v24. I hate my fucking life, this is scuffed, I have suicidal thoughts because of this shit and it has ruined my day

--- src/main/java/spigey/asteroide/modules/PlatformFlyModule.java
+++ src/main/java/spigey/asteroide/modules/PlatformFlyModule.java
@@ -3,6 +3,9 @@ package spigey.asteroide.modules;
 import meteordevelopment.meteorclient.events.meteor.KeyEvent;
 import meteordevelopment.meteorclient.events.world.TickEvent;
 import meteordevelopment.meteorclient.systems.modules.Module;
+import meteordevelopment.meteorclient.systems.modules.Modules;
+import meteordevelopment.meteorclient.systems.modules.misc.AutoRespawn;
+import meteordevelopment.meteorclient.systems.modules.movement.AirJump;
 import meteordevelopment.meteorclient.utils.misc.input.Input;
 import meteordevelopment.meteorclient.utils.misc.input.KeyAction;
 import meteordevelopment.orbit.EventHandler;
@@ -17,12 +20,22 @@ public class PlatformFlyModule extends Module {
     }
     private int level;
     private int tick;
+    private boolean plskms;
+    private final Module thing = Modules.get().get(AirJump.class);
 
     @Override
     public void onActivate() {
         assert mc.player != null;
         level = mc.player.getBlockPos().getY();
+        plskms = thing.isActive();
+        if(!thing.isActive()){thing.toggle();}
     }
+
+    @Override
+    public void onDeactivate() {
+        if(!plskms && thing.isActive()){thing.toggle();}
+    }
+
     @EventHandler
     private void onTick(TickEvent.Pre event){
         if(tick > 0) tick--;
@@ -36,8 +49,6 @@ public class PlatformFlyModule extends Module {
             /* mc.player.networkHandler.sendPacket(new PlayerMoveC2SPacket.PositionAndOnGround(mc.player.getX(), level, mc.player.getZ(), true));
             mc.player.setPosition(mc.player.getX(), level, mc.player.getZ()); */
             if(mc.player.getVelocity().y < 0) mc.player.setVelocity(mc.player.getVelocity().x, 0, mc.player.getVelocity().z);
-            mc.player.networkHandler.sendPacket(new PlayerMoveC2SPacket.PositionAndOnGround(mc.player.getX(), level, mc.player.getZ(), false));
-            mc.player.setPosition(mc.player.getX(), level, mc.player.getZ());
         }
     }
 }
