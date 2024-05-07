b98a674a53d8eeef94609e0ada188f94e6ea4e6f
DieZeitungsente <younes.haika@gmail.com>
2024-05-07 21:51:48 +0200

    v1, failed

---
+++ src/main/java/spigey/asteroide/modules/PlatformFlyModule.java
@@ -0,0 +1,36 @@
+package spigey.asteroide.modules;
+
+import meteordevelopment.meteorclient.events.meteor.KeyEvent;
+import meteordevelopment.meteorclient.events.world.TickEvent;
+import meteordevelopment.meteorclient.systems.modules.Module;
+import meteordevelopment.meteorclient.utils.misc.input.KeyAction;
+import meteordevelopment.orbit.EventHandler;
+import spigey.asteroide.AsteroideAddon;
+
+public class PlatformFlyModule extends Module {
+    public PlatformFlyModule() {
+        super(AsteroideAddon.CATEGORY, "platform-fly", "Stops you from falling");
+    }
+    private int level;
+
+    @Override
+    public void onActivate() {
+        assert mc.player != null;
+        level = mc.player.getBlockPos().getY();
+    }
+    @EventHandler
+    private void onKey(KeyEvent event){
+        if(event.action != KeyAction.Press) return;
+        if(mc.options.sneakKey.matchesKey(event.key, 0)) level--;
+        assert mc.player != null;
+        level = mc.player.getBlockPos().getY();
+    }
+    @EventHandler
+    private void onTick(TickEvent.Pre event){
+        assert mc.player != null;
+        if(mc.player.isOnGround()) return;
+        if(mc.player.getBlockPos().getY() == level) mc.player.jump();
+    }
+}
+
+
