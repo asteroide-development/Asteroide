ce09fcd0218eb4dfb7703949e84830b044977aa2
DieZeitungsente <younes.haika@gmail.com>
2024-05-07 22:57:22 +0200

    v12

--- src/main/java/spigey/asteroide/modules/PlatformFlyModule.java
+++ src/main/java/spigey/asteroide/modules/PlatformFlyModule.java
@@ -23,7 +23,7 @@ public class PlatformFlyModule extends Module {
     @EventHandler
     private void onKey(KeyEvent event){
         if(event.action != KeyAction.Press) return;
-        if(mc.options.sneakKey.matchesKey(event.key, 0)){level--; uwu = true; return;}
+        if(mc.options.sneakKey.matchesKey(event.key, 0)){level--; mc.player.setVelocity(mc.player.getVelocity().x, -1, mc.player.getVelocity().z); return;}
         if(!mc.options.jumpKey.matchesKey(event.key, 0)){level++; return;}
         assert mc.player != null;
         if(mc.player.isOnGround()) level = mc.player.getBlockPos().getY();
@@ -32,15 +32,10 @@ public class PlatformFlyModule extends Module {
     private void onTick(TickEvent.Pre event){
         assert mc.player != null;
         if(mc.player.isOnGround()) return;
-        if(mc.player.getBlockPos().getY() < level){
+        if(mc.player.getBlockPos().getY() <= level){
             /* mc.player.networkHandler.sendPacket(new PlayerMoveC2SPacket.PositionAndOnGround(mc.player.getX(), level, mc.player.getZ(), true));
             mc.player.setPosition(mc.player.getX(), level, mc.player.getZ()); */
             if(mc.player.getVelocity().y < 0) mc.player.setVelocity(mc.player.getVelocity().x, 0, mc.player.getVelocity().z);
-            if(uwu){
-                mc.player.networkHandler.sendPacket(new PlayerMoveC2SPacket.PositionAndOnGround(mc.player.getX(), level, mc.player.getZ(), true));
-                mc.player.setPosition(mc.player.getX(), level, mc.player.getZ());
-                uwu = false;
-            }
         }
     }
 }
