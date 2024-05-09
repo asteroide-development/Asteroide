142365c581f12cc0c465ebb13ee43445d92b2be7
DieZeitungsente <younes.haika@gmail.com>
2024-05-09 15:18:24 +0200

    creative flight

---
+++ src/main/java/spigey/asteroide/modules/CreativeFlightModule.java
@@ -0,0 +1,30 @@
+package spigey.asteroide.modules;
+
+import meteordevelopment.meteorclient.events.packets.PacketEvent;
+import meteordevelopment.meteorclient.mixin.PlayerMoveC2SPacketAccessor;
+import meteordevelopment.meteorclient.systems.modules.Module;
+import meteordevelopment.orbit.EventHandler;
+import spigey.asteroide.AsteroideAddon;
+
+public class CreativeFlightModule extends Module {
+    public CreativeFlightModule() {
+        super(AsteroideAddon.CATEGORY, "creative-flight", "Allows you to fly like you're in creative mode");
+    }
+
+    @Override
+    public void onActivate() {
+        assert mc.player != null;
+        mc.player.getAbilities().allowFlying = true;
+    }
+    @Override
+    public void onDeactivate() {
+        assert mc.player != null;
+        if(!mc.player.getAbilities().creativeMode) {mc.player.getAbilities().allowFlying = false; mc.player.getAbilities().flying = false;}
+    }
+    @EventHandler
+    private void onSendPacket(PacketEvent.Send event){
+        if(!(event.packet instanceof PlayerMoveC2SPacketAccessor)) return;
+        if(!isActive()) return;
+        ((PlayerMoveC2SPacketAccessor) event.packet).setOnGround(true);
+    }
+}
