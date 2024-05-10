8459ed5ce749f8e77796b6eb10e9aefa17197e5f
DieZeitungsente <younes.haika@gmail.com>
2024-05-10 19:35:55 +0200

    i forgor

--- src/main/java/spigey/asteroide/modules/MinehutAutoJoinRandomModule.java
+++ src/main/java/spigey/asteroide/modules/MinehutAutoJoinRandomModule.java
@@ -2,6 +2,7 @@ package spigey.asteroide.modules;
 
 import meteordevelopment.meteorclient.events.packets.PacketEvent;
 import meteordevelopment.meteorclient.systems.modules.Module;
+import meteordevelopment.orbit.EventHandler;
 import net.minecraft.network.packet.c2s.play.ClickSlotC2SPacket;
 import spigey.asteroide.AsteroideAddon;
 
@@ -9,7 +10,8 @@ public class MinehutAutoJoinRandomModule extends Module {
     public MinehutAutoJoinRandomModule() {
         super(AsteroideAddon.CATEGORY, "minehut-auto-join", "Automatically joins random minehut servers when in the lobby");
     }
-    private void onPacketSend(PacketEvent.Send event){
+    @EventHandler
+    private void onPacketSend(PacketEvent.Sent event){
         if(!(event.packet instanceof ClickSlotC2SPacket)) return;
         info(((ClickSlotC2SPacket) event.packet).getSlot() + ", " + ((ClickSlotC2SPacket) event.packet).getRevision() + ", " + ((ClickSlotC2SPacket) event.packet).getSyncId());
     }
