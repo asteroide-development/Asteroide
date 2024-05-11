4da5b7dff0c2d126a3d5bb1321ab738caf2d76ca
DieZeitungsente <younes.haika@gmail.com>
2024-05-11 14:17:25 +0200

    Fix AutoMacro

--- src/main/java/spigey/asteroide/modules/AutoMacro.java
+++ src/main/java/spigey/asteroide/modules/AutoMacro.java
@@ -39,19 +39,20 @@ public class AutoMacro extends Module {
     public AutoMacro() {
         super(AsteroideAddon.CATEGORY, "auto-macro", "Automatically runs a macro when a specified message is sent in the chat");
     }
-
+    String content;
     @EventHandler(priority = EventPriority.HIGHEST + 1)
     private void PacketReceive(PacketEvent.Receive event){
         banstuff();
         if(!isActive()) return;
-        if(!(event.packet instanceof ChatMessageS2CPacket)){return;}
-        String content = String.valueOf(event.packet.toString());
+        if(!(event.packet instanceof ChatMessageS2CPacket || event.packet instanceof GameMessageS2CPacket)) return;
+        if(event.packet instanceof ChatMessageS2CPacket) content = String.valueOf(event.packet.toString());
+        if(event.packet instanceof GameMessageS2CPacket) content = String.valueOf(((GameMessageS2CPacket) event.packet).content());
         for(int i = 0; i < messages.get().size(); i++){
             if(content.toLowerCase().contains(messages.get().get(i).toLowerCase())){
                 if(macro.get().get(i) != null){
                     msg(Config.get().prefix.get() + "macro " + macro.get().get(i));
                 } else{
-                    error("Error: Macro " + macro.get().get(i) + " is null");
+                    error("Error: Macro is null");
                 }
             }
         }
