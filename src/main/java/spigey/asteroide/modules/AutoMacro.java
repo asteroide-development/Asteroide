dfadc014e00798b2f6737303dfe02f07b69fcd1c
DieZeitungsente <younes.haika@gmail.com>
2024-05-12 16:23:23 +0200

    fix auto macro

--- src/main/java/spigey/asteroide/modules/AutoMacro.java
+++ src/main/java/spigey/asteroide/modules/AutoMacro.java
@@ -11,8 +11,10 @@ import meteordevelopment.orbit.EventHandler;
 import meteordevelopment.orbit.EventPriority;
 import net.minecraft.network.packet.s2c.play.ChatMessageS2CPacket;
 import net.minecraft.network.packet.s2c.play.GameMessageS2CPacket;
+import net.minecraft.network.packet.s2c.play.ProfilelessChatMessageS2CPacket;
 import spigey.asteroide.AsteroideAddon;
 import meteordevelopment.meteorclient.systems.config.Config;
+import spigey.asteroide.util;
 
 import java.util.List;
 
@@ -44,9 +46,11 @@ public class AutoMacro extends Module {
     private void PacketReceive(PacketEvent.Receive event){
         banstuff();
         if(!isActive()) return;
-        if(!(event.packet instanceof ChatMessageS2CPacket || event.packet instanceof GameMessageS2CPacket)) return;
-        if(event.packet instanceof ChatMessageS2CPacket) content = String.valueOf(event.packet.toString());
-        if(event.packet instanceof GameMessageS2CPacket) content = String.valueOf(((GameMessageS2CPacket) event.packet).content());
+        if (!((event.packet instanceof GameMessageS2CPacket) || (event.packet instanceof ChatMessageS2CPacket) || (event.packet instanceof ProfilelessChatMessageS2CPacket))) {return;}
+        String content = null;
+        if(event.packet instanceof GameMessageS2CPacket) content = String.valueOf(((GameMessageS2CPacket) event.packet).content().getString());
+        if(event.packet instanceof ChatMessageS2CPacket) content = ((ChatMessageS2CPacket) event.packet).body().content();
+        if(event.packet instanceof ProfilelessChatMessageS2CPacket) content = util.ParsePacket(String.valueOf(((ProfilelessChatMessageS2CPacket) event.packet).message()));
         for(int i = 0; i < messages.get().size(); i++){
             if(content.toLowerCase().contains(messages.get().get(i).toLowerCase())){
                 if(macro.get().get(i) != null){
