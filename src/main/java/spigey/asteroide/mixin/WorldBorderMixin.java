e8d33f639a4e17ec213712a8f8422bb0ae514874
DieZeitungsente <younes.haika@gmail.com>
2024-05-13 12:59:01 +0200

    shits sick omg

--- src/main/java/spigey/asteroide/mixin/WorldBorderMixin.java
+++ src/main/java/spigey/asteroide/mixin/WorldBorderMixin.java
@@ -1,19 +1,20 @@
 package spigey.asteroide.mixin;
 
-import meteordevelopment.meteorclient.systems.modules.Module;
 import meteordevelopment.meteorclient.systems.modules.Modules;
-import meteordevelopment.meteorclient.systems.modules.misc.AutoRespawn;
 import net.minecraft.entity.Entity;
-import net.minecraft.network.NetworkState;
 import net.minecraft.util.math.Box;
-import net.minecraft.util.math.MathHelper;
 import net.minecraft.world.border.WorldBorder;
 
 import org.spongepowered.asm.mixin.*;
 import spigey.asteroide.modules.BorderNoclipModule;
 
+import static meteordevelopment.meteorclient.MeteorClient.mc;
+
 @Mixin(WorldBorder.class)
 public abstract class WorldBorderMixin {
+
+    @Shadow
+    private double damagePerBlock;
 
     /**
      * @author
@@ -22,5 +23,17 @@ public abstract class WorldBorderMixin {
     @Overwrite
     public boolean canCollide(Entity entity, Box box) {
         return !Modules.get().get(BorderNoclipModule.class).isActive();
+    }
+    /**
+     * @author
+     * @reason
+     */
+    @Overwrite
+    public double getDamagePerBlock(){
+        if(!Modules.get().get(BorderNoclipModule.class).isActive()) {
+            assert mc.world != null;
+            return mc.world.getWorldBorder().getDamagePerBlock();
+        }
+        return Modules.get().get(BorderNoclipModule.class).damage.get();
     }
 }
--- src/main/java/spigey/asteroide/modules/BorderNoclipModule.java
+++ src/main/java/spigey/asteroide/modules/BorderNoclipModule.java
@@ -1,6 +1,10 @@
 package spigey.asteroide.modules;
 
 import meteordevelopment.meteorclient.events.world.TickEvent;
+import meteordevelopment.meteorclient.settings.DoubleSetting;
+import meteordevelopment.meteorclient.settings.IntSetting;
+import meteordevelopment.meteorclient.settings.Setting;
+import meteordevelopment.meteorclient.settings.SettingGroup;
 import meteordevelopment.meteorclient.systems.modules.Module;
 import meteordevelopment.orbit.EventHandler;
 import net.minecraft.entity.Entity;
@@ -12,6 +16,15 @@ public class BorderNoclipModule extends Module {
     public BorderNoclipModule() {
         super(AsteroideAddon.CATEGORY, "border-noclip", "Removes the world border collision");
     }
+    private final SettingGroup sgGeneral = settings.getDefaultGroup();
+    public final Setting<Double> damage = sgGeneral.add(new DoubleSetting.Builder()
+        .name("damage-per-block")
+        .description("Damage dealt per block when outside the world border")
+        .defaultValue(0)
+        .min(0)
+        .sliderMax(10)
+        .build()
+    );
 }
 
 
