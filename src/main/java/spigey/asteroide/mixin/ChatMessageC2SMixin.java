c8e9f116d8e5a5723fb25dc8bd4c2b58aeebeb1c
DieZeitungsente <younes.haika@gmail.com>
2024-05-13 22:17:31 +0200

    it does not work btw

--- src/main/java/spigey/asteroide/mixin/ChatMessageC2SMixin.java
+++ src/main/java/spigey/asteroide/mixin/ChatMessageC2SMixin.java
@@ -1,31 +1,27 @@
 package spigey.asteroide.mixin;
 
-import io.netty.buffer.Unpooled;
-import net.minecraft.network.PacketByteBuf;
-import net.minecraft.network.message.LastSeenMessageList;
-import net.minecraft.network.message.MessageSignatureData;
+import meteordevelopment.meteorclient.systems.modules.Modules;
 import net.minecraft.network.packet.c2s.play.ChatMessageC2SPacket;
+import net.minecraft.world.border.WorldBorder;
+import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Unique;
+import org.spongepowered.asm.mixin.Overwrite;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.Redirect;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
-
-import java.time.Instant;
+import org.spongepowered.asm.mixin.injection.ModifyArg;
+import spigey.asteroide.modules.WordFilterModule;
 
 import static meteordevelopment.meteorclient.utils.player.ChatUtils.info;
 
 @Mixin(ChatMessageC2SPacket.class)
-public class ChatMessageC2SMixin {
-    @Unique
-    private String content;
-
-    @Inject(method = "<init>(Ljava/lang/String;Ljava/time/Instant;JLnet/minecraft/network/message/MessageSignatureData;Lnet/minecraft/network/message/LastSeenMessageList$Acknowledgment;)V", at = @At("TAIL"), cancellable = true)
-    private void onChatMessagePacketConstructed(String content, Instant timestamp, long salt, MessageSignatureData signature, LastSeenMessageList.Acknowledgment acknowledgment, CallbackInfo ci) {
-        this.content = "ermm what the sigma";
-        System.out.println("ermm what the sigma");
-        info("ermm what the flip");
-        ci.cancel();
-    }
+public abstract class ChatMessageC2SMixin {
+    /**
+     * @author Spigey
+     * @reason Word Filter
+     *
+    @Overwrite
+    public String chatMessage(){
+        if(Modules.get().get(WordFilterModule.class).isActive()) return "WOAH DUDE!!! IT WORKED!!!111";
+        return "ermm what the sigma.";
+    } */
 }
