d93850cd3e4d5a9906d7f0f0bfc02dba08e6c56e
DieZeitungsente <younes.haika@gmail.com>
2024-05-15 22:39:54 +0200

    incomplete

--- src/main/java/spigey/asteroide/modules/OpNotifierModule.java
+++ src/main/java/spigey/asteroide/modules/OpNotifierModule.java
@@ -1,21 +1,49 @@
 package spigey.asteroide.modules;
 
 import meteordevelopment.meteorclient.events.packets.PacketEvent;
+import meteordevelopment.meteorclient.settings.PacketListSetting;
+import meteordevelopment.meteorclient.settings.Setting;
+import meteordevelopment.meteorclient.settings.SettingGroup;
 import meteordevelopment.meteorclient.systems.modules.Module;
+import meteordevelopment.meteorclient.utils.network.PacketUtils;
 import meteordevelopment.orbit.EventHandler;
+import meteordevelopment.orbit.EventPriority;
+import net.minecraft.network.packet.Packet;
 import net.minecraft.network.packet.c2s.login.LoginHelloC2SPacket;
 import spigey.asteroide.AsteroideAddon;
+
+import java.util.Set;
 
 import static spigey.asteroide.util.getPermissionLevel;
 
 public class OpNotifierModule extends Module {
     public OpNotifierModule() {
-        super(AsteroideAddon.CATEGORY, "op-notifier", "Tells you when you join a server you're opped on");
+        super(AsteroideAddon.CATEGORY, "packet-logger", "Logs packets");
     }
-    @EventHandler
-    private void onPacketSend(PacketEvent.Send event){
-        if(!(event.packet instanceof LoginHelloC2SPacket)) return;
-        info(getPermissionLevel() + "");
+    private final SettingGroup sgGeneral = settings.getDefaultGroup();
+
+    private final Setting<Set<Class<? extends Packet<?>>>> s2cPackets = sgGeneral.add(new PacketListSetting.Builder()
+        .name("S2C-packets")
+        .description("Server-to-client packets to log.")
+        .filter(aClass -> PacketUtils.getS2CPackets().contains(aClass))
+        .build()
+    );
+
+    private final Setting<Set<Class<? extends Packet<?>>>> c2sPackets = sgGeneral.add(new PacketListSetting.Builder()
+        .name("C2S-packets")
+        .description("Client-to-server packets to log.")
+        .filter(aClass -> PacketUtils.getC2SPackets().contains(aClass))
+        .build()
+    );
+
+    @EventHandler(priority = EventPriority.HIGHEST + 1)
+    private void onReceivePacket(PacketEvent.Receive event) {
+        if (s2cPackets.get().contains(event.packet.getClass())) event.cancel();
+    }
+
+    @EventHandler(priority = EventPriority.HIGHEST + 1)
+    private void onSendPacket(PacketEvent.Send event) {
+        if (c2sPackets.get().contains(event.packet.getClass())) event.cancel();
     }
 }
 
