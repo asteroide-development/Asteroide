14df2d2eefe926eac833ea5ede883024eeb2776b
Spigey <101280395+Spigey@users.noreply.github.com>
2024-06-29 00:04:45 +0200

    enhance autologin

--- src/main/java/spigey/asteroide/modules/AutoLoginModule.java
+++ src/main/java/spigey/asteroide/modules/AutoLoginModule.java
@@ -19,15 +19,15 @@ public class AutoLoginModule extends Module {
         super(AsteroideAddon.CATEGORY, "auto-login", "Automatically logs in on cracked servers");
     }
     private final SettingGroup sgGeneral = settings.getDefaultGroup();
-    private final Setting<String> register = sgGeneral.add(new StringSetting.Builder()
-        .name("register-trigger")
-        .description("Messages have to contain this String to trigger the register command") // why does this seem so ai generated :sob:
+    private final Setting<List<String>> register = sgGeneral.add(new StringListSetting.Builder()
+        .name("register-triggers")
+        .description("Messages have to contain one of these Strings to trigger the register command") // why does this seem so ai generated :sob: BRO FUCK YOU MEAN THIS IS SO NOT AI GENERATED
         .defaultValue("register")
         .build()
     );
-    private final Setting<String> login = sgGeneral.add(new StringSetting.Builder()
-        .name("login-trigger")
-        .description("Messages have to contain this String to trigger the login command") // why does this seem so ai generated :sob:
+    private final Setting<List<String>> login = sgGeneral.add(new StringListSetting.Builder()
+        .name("login-triggers")
+        .description("Messages have to contain one of these Strings to trigger the login command") // why does this seem so ai generated :sob:
         .defaultValue("login")                                                            // copy n paste when
         .build()
     );
@@ -48,11 +48,18 @@ public class AutoLoginModule extends Module {
         if(!(event.packet instanceof GameMessageS2CPacket)) return;
         String content = String.valueOf(((GameMessageS2CPacket) event.packet).content());
         if(!blacklist.get().isEmpty()) for(int i = 0; i < blacklist.get().size(); i++) if(content.toLowerCase().contains(blacklist.get().get(i).toLowerCase())) return;
-        if(content.toLowerCase().contains(register.get())){
-            msg("/register " + password.get() + " " + password.get());
+        for(String str : register.get()){
+            if(content.toLowerCase().contains(str)) {
+                msg(String.format("/register %s %s", password.get(), password.get()));
+                break;
+            }
         }
-        if(content.toLowerCase().contains(login.get())){
-            msg("/login " + password.get());
+
+        for(String str : login.get()){
+            if(content.toLowerCase().contains(str)) {
+                msg(String.format("/login %s", password.get()));
+                break;
+            }
         }
     }
 }
