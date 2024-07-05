d8c750d8e34276b4f2011b02794158be1ab07279
Spigey <101280395+Spigey@users.noreply.github.com>
2024-07-05 13:43:31 +0200

    thing

--- src/main/java/spigey/asteroide/modules/FastStaircaseModule.java
+++ src/main/java/spigey/asteroide/modules/FastStaircaseModule.java
@@ -2,6 +2,8 @@ package spigey.asteroide.modules;
 
 import meteordevelopment.meteorclient.events.world.TickEvent;
 import meteordevelopment.meteorclient.systems.modules.Module;
+import meteordevelopment.meteorclient.systems.modules.Modules;
+import meteordevelopment.meteorclient.systems.modules.movement.HighJump;
 import meteordevelopment.orbit.EventHandler;
 import net.minecraft.block.StairsBlock;
 import net.minecraft.util.math.Direction;
@@ -15,6 +17,9 @@ public class FastStaircaseModule extends Module {
     private void onTick(TickEvent.Post event){
         if(!isActive()){return;}
         if(!Registries.BLOCK.getId(mc.world.getBlockState(mc.player.getBlockPos()).getBlock()).toString().endsWith("_stairs")) return;
-        if(mc.player.getHorizontalFacing() == mc.world.getBlockState(mc.player.getBlockPos()).get(StairsBlock.FACING) && mc.player.input.movementForward > 0) mc.player.jump();
+        boolean asd = Modules.get().get(HighJump.class).isActive();
+        if(asd) Modules.get().get(HighJump.class).toggle();
+        if(Registries.BLOCK.getId(mc.world.getBlockState(mc.player.getBlockPos()).getBlock()).toString().endsWith("_stairs")) if(mc.player.getHorizontalFacing() == mc.world.getBlockState(mc.player.getBlockPos()).get(StairsBlock.FACING) && mc.player.input.movementForward > 0) mc.player.jump();
+        if(asd) Modules.get().get(HighJump.class).toggle();
     }
 }
