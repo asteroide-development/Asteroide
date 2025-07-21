972aab9fd275e0a0c17c32d67cbdb7521fdaf950
Spigey <101280395+Spigey@users.noreply.github.com>
2025-07-21 22:07:21 +0200

    Fix .close & Update JAR

--- src/main/java/spigey/asteroide/commands/CloseCommand.java
+++ src/main/java/spigey/asteroide/commands/CloseCommand.java
@@ -3,6 +3,7 @@ package spigey.asteroide.commands;
 import com.mojang.brigadier.builder.LiteralArgumentBuilder;
 import meteordevelopment.meteorclient.commands.Command;
 import net.minecraft.command.CommandSource;
+import net.minecraft.network.packet.c2s.play.CloseHandledScreenC2SPacket;
 
 public class CloseCommand extends Command {
     public CloseCommand() {
@@ -12,7 +13,11 @@ public class CloseCommand extends Command {
     @Override
     public void build(LiteralArgumentBuilder<CommandSource> builder) {
         builder.executes(context -> {
-            try{mc.player.closeScreen();}catch(Exception L){/**/}
+            try{
+                int syncId = mc.player.currentScreenHandler.syncId;
+                mc.player.closeScreen();
+                mc.player.networkHandler.sendPacket(new CloseHandledScreenC2SPacket(syncId));
+            }catch(Exception L){/**/}
             return SINGLE_SUCCESS;
         });
     }
