9d9cdba5a7f6626ae30edd2d73d66e0a3a51b74b
Spigey <101280395+Spigey@users.noreply.github.com>
2025-08-22 10:59:03 +0200

    Remove ChatMessageC2SMixin, add commands/chat filter to typo & update jar

--- src/main/java/spigey/asteroide/mixin/ClientPlayNetworkHandlerMixin.java
+++ src/main/java/spigey/asteroide/mixin/ClientPlayNetworkHandlerMixin.java
@@ -12,7 +12,7 @@ public class ClientPlayNetworkHandlerMixin {
     @ModifyVariable(method = "sendChatCommand(Ljava/lang/String;)V", at = @At("HEAD"), argsOnly = true)
     private String modifyCommand(String command) {
         TypoModule typo = Modules.get().get(TypoModule.class);
-        if(!typo.isActive()) return command;
+        if(!typo.isActive() || !typo.commands.get()) return command;
         for(int i = 0; i < typo.keywords.get().size(); i++) {
             if (!command.toLowerCase().contains(typo.keywords.get().get(i).toLowerCase())) continue;
             if(typo.replacements.get().size() < i) break;
--- src/main/java/spigey/asteroide/modules/TypoModule.java
+++ src/main/java/spigey/asteroide/modules/TypoModule.java
@@ -2,6 +2,7 @@ package spigey.asteroide.modules;
 
 import meteordevelopment.meteorclient.events.game.SendMessageEvent;
 import meteordevelopment.meteorclient.events.packets.PacketEvent;
+import meteordevelopment.meteorclient.settings.BoolSetting;
 import meteordevelopment.meteorclient.settings.Setting;
 import meteordevelopment.meteorclient.settings.SettingGroup;
 import meteordevelopment.meteorclient.settings.StringListSetting;
@@ -20,9 +21,12 @@ public class TypoModule extends Module {
     private final SettingGroup sgGeneral = settings.getDefaultGroup();
     public final Setting<List<String>> keywords = sgGeneral.add(new StringListSetting.Builder().name("keywords").description("Keywords to replace").defaultValue("ex1", "ex2").build());
     public final Setting<List<String>> replacements = sgGeneral.add(new StringListSetting.Builder().name("replacements").description("Text to replace the keywords with").defaultValue("ex1 will be replaced with this text", "ex2 will be replaced with this text").build());
+    public final Setting<Boolean> commands = sgGeneral.add(new BoolSetting.Builder().name("replace commands").description("Replace text in commands.").defaultValue(true).build());
+    public final Setting<Boolean> chat = sgGeneral.add(new BoolSetting.Builder().name("replace chat").description("Replace text in chat messages.").defaultValue(true).build());
 
     @EventHandler
     private void onMessageSend(SendMessageEvent event){
+        if (!chat.get() || !isActive()) return;
         String content = event.message;
         for(int i = 0; i < keywords.get().size(); i++) {
             if (!content.toLowerCase().contains(keywords.get().get(i).toLowerCase())) continue;
