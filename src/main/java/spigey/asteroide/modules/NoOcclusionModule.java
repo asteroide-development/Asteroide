329f4f60ada87f084ac6f3c20c1f6b96cfa94c9d
Spigey <101280395+Spigey@users.noreply.github.com>
2025-08-24 19:00:27 +0200

    add light level setting to No Occlusion & update jar

--- src/main/java/spigey/asteroide/modules/NoOcclusionModule.java
+++ src/main/java/spigey/asteroide/modules/NoOcclusionModule.java
@@ -1,6 +1,9 @@
 package spigey.asteroide.modules;
 
 import meteordevelopment.meteorclient.events.world.AmbientOcclusionEvent;
+import meteordevelopment.meteorclient.settings.IntSetting;
+import meteordevelopment.meteorclient.settings.Setting;
+import meteordevelopment.meteorclient.settings.SettingGroup;
 import meteordevelopment.meteorclient.systems.modules.Module;
 import meteordevelopment.orbit.EventHandler;
 import spigey.asteroide.AsteroideAddon;
@@ -10,9 +13,15 @@ public class NoOcclusionModule extends Module {
         super(AsteroideAddon.CATEGORY, "No-Occlusion", "Removes ambient occlusion.");
     }
 
+    private final SettingGroup sgGeneral = settings.getDefaultGroup();
+    private final Setting<Integer> lightLevel = sgGeneral.add(new IntSetting.Builder().name("Light Level").description("The light level to set.").defaultValue(1).sliderMin(0).sliderMax(15).onChanged((value) -> mc.worldRenderer.reload()).build());
+
+    @Override public void onActivate(){ mc.worldRenderer.reload(); }
+    @Override public void onDeactivate(){ mc.worldRenderer.reload(); }
+
     @EventHandler
     private void onAmbientOcclusion(AmbientOcclusionEvent event){ // wow, this was so hard to code!
         if(!isActive()) return;
-        event.lightLevel = 1;
+        event.lightLevel = lightLevel.get();
     }
 }
