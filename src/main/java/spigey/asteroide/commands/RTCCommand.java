7c9f689d10f3852b36841aa359238158fdcd4877
Spigey <101280395+Spigey@users.noreply.github.com>
2025-09-01 13:52:17 +0200

    Add .rtc discord & update jar

--- src/main/java/spigey/asteroide/commands/RTCCommand.java
+++ src/main/java/spigey/asteroide/commands/RTCCommand.java
@@ -3,8 +3,18 @@ package spigey.asteroide.commands;
 import com.mojang.brigadier.arguments.StringArgumentType;
 import com.mojang.brigadier.builder.LiteralArgumentBuilder;
 import meteordevelopment.meteorclient.commands.Command;
+import meteordevelopment.meteorclient.commands.Commands;
 import meteordevelopment.meteorclient.systems.modules.Modules;
+import meteordevelopment.meteorclient.utils.misc.text.MeteorClickEvent;
+import net.minecraft.block.entity.BlockEntity;
+import net.minecraft.block.entity.MobSpawnerBlockEntity;
 import net.minecraft.command.CommandSource;
+import net.minecraft.component.Component;
+import net.minecraft.text.ClickEvent;
+import net.minecraft.text.HoverEvent;
+import net.minecraft.text.MutableText;
+import net.minecraft.text.Text;
+import net.minecraft.util.Formatting;
 import spigey.asteroide.AsteroideAddon;
 import spigey.asteroide.modules.RTCSettingsModule;
 import spigey.asteroide.utils.ws;
@@ -34,6 +44,25 @@ public class RTCCommand extends Command {
             AsteroideAddon.showRtc = !willHide;
             return SINGLE_SUCCESS;
         }));
+        builder.then(literal("discord").executes(ctx ->{
+            mc.player.sendMessage(getButton(Text.literal("§8§l[§c§lAsteroide§8§l]§7 Join our Discord")), false);
+            return SINGLE_SUCCESS;
+        }));
         builder.then(literal("keep typing to send a message").executes(ctx -> SINGLE_SUCCESS));
+    }
+
+    private static MutableText getButton(MutableText message){
+        MutableText Button = Text.literal("§8[ §9HERE §8]");
+        Button.setStyle(Button.getStyle()
+            .withClickEvent(new MeteorClickEvent(
+                ClickEvent.Action.OPEN_URL,
+                "https://discord.gg/QFzE3UzdpQ"
+            ))
+            .withHoverEvent(new HoverEvent(
+                HoverEvent.Action.SHOW_TEXT,
+                Text.literal("§7discord.gg/QFzE3UzdpQ")
+            ))
+        );
+        return message.append(" ").append(Button);
     }
 }
